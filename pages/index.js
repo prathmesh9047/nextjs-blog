a6ac2dae0e05f6f77375eb09c1beec860c9b26d6
Govalkar <prathmesh.govalkar@bauer.de>
2023-09-15 18:34:03 +0530

    new changes

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Prathmesh. I'm a software engineer.</p>
+        <p>Hello, I'm Prathmesh Govalkar. I'm a software engineer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
